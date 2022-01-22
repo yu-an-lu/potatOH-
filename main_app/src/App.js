1d0d5e6371999a808fff92e284bea111f1e0ad8a
samperezh <samantha.perezhoffman@mail.mcgill.ca>
2022-01-22 17:32:30 -0500

    Create a table using React in App.js

--- main_app/src/App.js
+++ main_app/src/App.js
@@ -1,49 +1,94 @@
-import logo from './logo.svg';
+
+import React , { useState } from "react";
+
 import './App.css';
-import './aesthetic.css'
+//import './aesthetic.css'
 
-//import ScriptTag from 'react-script-tag';
-
+import data from "./demo_data.json"; //takes the data
 
 
 function App() {
 
+  //stores it in state, state is available via food variable, can update the state via setFood function
+  const [foods, setFoods] = useState(data); 
+
+  const[addDataToForm, setAddDataToForm] = useState({
+    //initialize this to be an object
+    //going to use the name of the input to determine what input field and input value has changed 
+    productName: '',
+    expirationDate: '',
+    email: '',
+  })
+
+  const handleAddFormChange = (event) => {
+    event.preventDefault();
+
+    const fieldName = event.target.getAttribute('name');
+    const fieldValue = event.target.value;
+
+    const newFormData = { ...addDataToForm};
+    newFormData[fieldName] = fieldValue;
+
+    setAddDataToForm(newFormData);
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           POTATOH!!! <br></br>
-
         </p>
-
       </header>
       <body>
-
-        <div id="container">
-        <div id="productDiv"class="input">Product:<input id="Product" type="text" placeholder="JS" />
-        </div>
-        <div class="input">ExpirationDate:<input id="Expiration Date" type="text" placeholder="231" />
-        </div>
-        
-        <button id ="entry">Input entry</button>
-        </div>
-
-        <table id="display">
-          <tr>
-            <th>Product</th>
-            <th>ExpirationDate</th>
-
-          </tr>
-
+        <table>
+          <thead>
+            <tr>
+              <th>Product Name</th>
+              <th>Expiration Date</th>
+              <th>Product Category</th>
+            </tr>
+          </thead>
+          <tbody>
+            { //going to create a row for each food in our foods array 
+              //parse through array, current element will be temp stored in variable food
+            foods.map((food)=> (           
+            <tr>
+              <td>{food.productName}</td>
+              <td>{food.expirationDate}</td>
+              <td>{food.productCategory}</td>
+            </tr>
+            ))}
+          </tbody>
         </table>
-        <script src = "interaction.js"></script>
+        <h2>Add a Product</h2>
+        <form>
+          <input 
+            type="text" 
+            name="productName" 
+            required="requiered" 
+            placeholder="Enter a product..."
+            onChange={handleAddFormChange}
+            />
+            <input 
+            type="date" 
+            name="expirationDate" 
+            required="requiered" 
+            placeholder="Enter an expiration date..."
+            onChange={handleAddFormChange}
+            />
+            <input 
+            type="text" 
+            name="productCategory" 
+            required="requiered" 
+            placeholder="Enter a product category..."
+            onChange={handleAddFormChange}
+            />
+            <button type="submit">Add to fridge</button>
+        </form>
       </body>
     </div>
   );
 }
 
 export default App;
-//const interaction = props => (
-//  <ScriptTag type="text/javascript" src = "interaction.js" />
-//)
