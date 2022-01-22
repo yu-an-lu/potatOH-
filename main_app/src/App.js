6886579dd13d4d116e87f8cdb651272fa0aa8299
samperezh <samantha.perezhoffman@mail.mcgill.ca>
2022-01-22 13:41:16 -0500

    clearing up my_app

--- main_app/src/App.js
+++ main_app/src/App.js
@@ -14,19 +14,8 @@ function App() {
         <p>
           POTATOH!!! <br></br>
 
-          let dataForTable = [];
-          <Table data = {dataForTable} /> //creates Table component
-
-          Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
