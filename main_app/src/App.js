32eae5e31f1da02b21a57cf1cf0865555a09a790
samperezh <samantha.perezhoffman@mail.mcgill.ca>
2022-01-22 15:33:06 -0500

    added html components from table.html to App.js
    The elements are not added  in the table though

--- main_app/src/App.js
+++ main_app/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import './aesthetic.css'
 
-import './Table.js';
-import './Food.js';
+//import ScriptTag from 'react-script-tag';
 
 
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,8 +18,32 @@ function App() {
         </p>
 
       </header>
+      <body>
+
+        <div id="container">
+        <div id="productDiv"class="input">Product:<input id="Product" type="text" placeholder="JS" />
+        </div>
+        <div class="input">ExpirationDate:<input id="Expiration Date" type="text" placeholder="231" />
+        </div>
+        
+        <button id ="entry">Input entry</button>
+        </div>
+
+        <table id="display">
+          <tr>
+            <th>Product</th>
+            <th>ExpirationDate</th>
+
+          </tr>
+
+        </table>
+        <script src = "interaction.js"></script>
+      </body>
     </div>
   );
 }
 
 export default App;
+//const interaction = props => (
+//  <ScriptTag type="text/javascript" src = "interaction.js" />
+//)
